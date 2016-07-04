1fd99078eca1c96d84a9388dbe60116cab4405df
Lawrence Forooghian <lawrence.forooghian@gmail.com>
2016-07-05 00:01:31 +0100

    Make some improvements to Hammerspoon config

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,3 +1,11 @@
+-- Configure Hammerspoon
+
+hs.autoLaunch(true)
+hs.automaticallyCheckForUpdates(true)
+hs.consoleOnTop(false)
+hs.dockIcon(false)
+hs.menuIcon(false)
+
 -- Everything in here is pretty much from the tutorial: http://www.hammerspoon.org/go/
 
 -- First, reproduce the SizeUp commands:
@@ -77,30 +85,61 @@ if caffeine then
 end
 
 --- Automatically switch to most appropriate layout for connected keyboard.
-function setMostAppropriateKeyboardLayout()
+
+local logger = hs.logger.new("keyboard-layout-switcher", "debug")
+
+function setMostAppropriateKeyboardLayout(loggingReason)
     devices = hs.usb.attachedDevices()
 
     for key, value in pairs(devices) do
         --- My Filco Majestouch 2 UK
         if value["vendorID"] == 0x04d9 and value["productID"] == 0x1818 then
-            hs.keycodes.setLayout("British - PC")
+            setKeyboardLayoutIfNeeded("British - PC", loggingReason)
             return
         end
     end
 
-    hs.keycodes.setLayout("British")
+    setKeyboardLayoutIfNeeded("British", loggingReason)
+end
+
+function setKeyboardLayoutIfNeeded(name, loggingReason)
+    if hs.keycodes.currentLayout() ~= name then
+        if hs.keycodes.setLayout(name) then
+            logger.f("Set keyboard layout to %s. (reason: %s)", name, loggingReason)
+        else
+            logger.ef("Failed to set keyboard layout to %s. (reason: %s)", name, loggingReason)
+        end
+        hs.openConsole() --- For debug purposes until I get this working properly
+    end
 end
 
 function usbDevicesChanged(info)
-    setMostAppropriateKeyboardLayout()
+    setMostAppropriateKeyboardLayout("usbDevicesChanged")
 end
 
 local usbWatcher = hs.usb.watcher.new(usbDevicesChanged)
 usbWatcher:start()
 
 function powerEventHappened(info)
-    setMostAppropriateKeyboardLayout()
+    setMostAppropriateKeyboardLayout("powerEventHappened")
 end
 
 local caffeinateWatcher = hs.caffeinate.watcher.new(powerEventHappened)
 caffeinateWatcher:start()
+
+--- Add a menu icon for starting the screensaver (I can't find another obvious way of locking the screen without putting the computer to sleep)
+
+local lockMenu = hs.menubar.new()
+lockMenu:setTitle(hs.utf8.codepointToUTF8("U+1F4AA"))
+
+function lockMenuClicked()
+    -- TODO Investigate: these don't seem to actually work; need to set them in System Preferences
+    --hs.execute("defaults write com.apple.screensaver askForPassword -boolean true")
+    --hs.execute("defaults write com.apple.screensaver askForPasswordDelay -integer 5")
+
+    hs.execute("pmset displaysleepnow")
+end
+
+if lockMenu then
+    lockMenu:setClickCallback(lockMenuClicked)
+end
