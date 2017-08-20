bbe13e5bf52b0d4c40439ae62891b792c24c0996
Lawrence Forooghian <lawrence@Lawrences-MacBook-Pro.local>
2017-08-20 20:55:52 +0100

    Add script to install everything

---
+++ install.sh
@@ -0,0 +1,24 @@
+cd ~
+
+for i in .hammerspoon .tmux.conf .vimperatorrc .vimrc .zsh_profile .zshrc
+do
+	ln -s dotfiles/$i $i
+done
+
+git config --global core.excludesfile ~/dotfiles/global.gitignore
+
+brew install ack tmux tree vim watchman reattach-to-user-namespace ncdu
+brew cask install transmission vlc anki hammerspoon mactex
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+chsh -s /bin/zsh 
+
+git config --global user.name "Lawrence Forooghian"
+git config --global user.email "lawrence.forooghian@gmail.com"
+
+vim -c BundleInstall
+
+cd .vim/bundle/command-t/ruby/command-t/ext/command-t
+/usr/local/bin/ruby extconf.rb
+make install
