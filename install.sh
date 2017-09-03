59d2fbf679179dda36975016962533135d655ada
Lawrence Forooghian <lawrence.forooghian@gmail.com>
2017-09-03 18:33:15 +0100

    Add Virtualbox to install script

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ git config --global core.excludesfile ~/dotfiles/global.gitignore
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew install ack tmux tree vim watchman reattach-to-user-namespace ncdu node youtube-dl ffmpeg mas
-brew cask install transmission vlc anki hammerspoon mactex spotmenu spotify skype whatsapp 1password dropbox
+brew cask install transmission vlc anki hammerspoon mactex spotmenu spotify skype whatsapp 1password dropbox virtualbox
 npm -g install instant-markdown-d
 
 mas account # `mas signin` doesn't work with 2FA
