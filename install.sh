111a3809e663a89b71a0b2e86954a0c9f4ef4451
Lawrence Forooghian <lawrence.forooghian@gmail.com>
2019-08-04 13:02:43 +0100

    Remove some stuff not needed for dxw

--- install.sh
+++ install.sh
@@ -17,21 +17,16 @@ git config --global core.excludesfile ~/dotfiles/global.gitignore
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install ack tmux tree vim watchman reattach-to-user-namespace ncdu node youtube-dl ffmpeg mas emacs rbenv
-brew cask install transmission vlc anki hammerspoon mactex spotmenu spotify skype whatsapp 1password dropbox virtualbox iterm2
+brew install ack tmux tree vim reattach-to-user-namespace ncdu node emacs rbenv
+brew cask install hammerspoon spotify iterm2
 npm -g install instant-markdown-d
-
-mas account # `mas signin` doesn't work with 2FA
-mas install 410628904 # Wunderlist
-mas install 406056744 # Evernote
-mas install 803453959 # Slack
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 chsh -s /bin/zsh 
 
 git config --global user.name "Lawrence Forooghian"
-git config --global user.email "lawrence.forooghian@gmail.com"
+git config --global user.email "lawrence@dxw.com"
 
 vim -c BundleInstall -c q -c q
 
@@ -39,5 +34,4 @@ cd .vim/bundle/command-t/ruby/command-t/ext/command-t
 /usr/local/bin/ruby extconf.rb
 make install
 
-open -a SpotMenu
 open -a Hammerspoon
