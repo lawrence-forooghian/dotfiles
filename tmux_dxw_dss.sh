69a26b7d4452cd25ac942782f25ae16ae5a5af76
Lawrence Forooghian <lawrence@dxw.com>
2019-09-02 18:05:18 +0100

    Tidy up DSS tmux script

--- tmux_dxw_dss.sh
+++ tmux_dxw_dss.sh
@@ -11,17 +11,18 @@ then
   exit 1
 else
   # Create the session
-  tmux new-session -s ${SESSION_NAME} -n TODO -d
+  tmux new-session -s ${SESSION_NAME} -d
 
   index=0
+  target="${SESSION_NAME}:${index}"
 
   # Create window for notes
   cd ~/org
-  tmux new-window -n notes -t "${SESSION_NAME}"
-  ((index++))
+  tmux rename-window -t "${TARGET}" notes
+  tmux send-keys -t "${TARGET}" 'cd ~/org' 'C-m'
   tmux send-keys -t "${TARGET}" emacs C-m
 
-  target="${SESSION_NAME}:${index}"
+  ((index++))
 
   # Create window for each service, with splits and vim in the largest pane
   for service in DataSubmissionService DataSubmissionServiceAPI
@@ -42,10 +43,7 @@ else
   tmux new-window -n manual -a -t "${SESSION_NAME}"
   ((index++))
 
-  # Kill the initial window
-  tmux kill-window -t "${SESSION_NAME}:0"
-
   # Start out on the first window when we attach
-  tmux select-window -t "${SESSION_NAME}:1"
+  tmux select-window -t "${SESSION_NAME}:0"
 fi
 tmux attach -t ${SESSION_NAME}
