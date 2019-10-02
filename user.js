179dfd4c6ed2b495d0711ed38ef700f772d7565c
Lawrence Forooghian <lawrence.forooghian@gmail.com>
2019-10-03 00:03:44 +0100

    Add Firefox user.js

---
+++ user.js
@@ -0,0 +1,8 @@
+user_pref("browser.newtabpage.activity-stream.feeds.section.highlights", false);
+user_pref("browser.newtabpage.activity-stream.feeds.snippets", false);
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+user_pref("browser.newtabpage.activity-stream.prerender", false);
+user_pref("browser.newtabpage.activity-stream.showSearch", false);
+
+// For full-screen in macOS
+user_pref("browser.tabs.closeWindowWithLastTab", false);
