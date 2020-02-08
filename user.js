615be5f44ba3f9b34a31893a455ffa1ab1bd5082
Lawrence Forooghian <lawrence.forooghian@gmail.com>
2020-02-08 14:41:59 +0000

    update user.js even though it’s not actually being used

--- user.js
+++ user.js
@@ -5,6 +5,7 @@ user_pref("browser.newtabpage.activity-stream.prerender", false);
 user_pref("browser.newtabpage.activity-stream.showSearch", false);
 
 // For full-screen in macOS
-user_pref("browser.tabs.closeWindowWithLastTab", false);
+// user_pref("browser.tabs.closeWindowWithLastTab", false);
 
 user_pref("mousewheel.with_meta.action", 1);
+user_pref("mousewheel.with_control.action", 1);
