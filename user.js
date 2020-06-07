66ccc6747804d014d00a8781d12161d7118195bf
Lawrence Forooghian <lawrence@forooghian.com>
2020-06-07 02:38:10 +0100

    document user.js

--- user.js
+++ user.js
@@ -1,3 +1,10 @@
+// Installation: make a symlink from this to the profile directory, something like
+// ~/Library/Application Support/Firefox/Profiles/c8csnb2o.default-release-1592253731292
+
+// Confirm it's working by looking at "user.js Preferences" in about:support
+
+// Changes made in Firefox will _not_ be persisted to this file. They will be lost on next launch of Firefox.
+
 user_pref("browser.newtabpage.activity-stream.feeds.section.highlights", false);
 user_pref("browser.newtabpage.activity-stream.feeds.snippets", false);
 user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
