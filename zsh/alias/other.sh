57058ca8912c022edfc416c79255dcdba0f7f813
Lawrence Forooghian <lawrence@forooghian.com>
2023-11-09 09:26:20 -0300

    Update Ably tmuxinator config

--- zsh/alias/other.sh
+++ zsh/alias/other.sh
@@ -4,7 +4,7 @@ alias lsa='ls -alh'
 alias less='less -R' # colors
 
 alias notes='tnr notes && tmux at -t notes'
-alias ably='tnr ably/ably && tnr ably/ably-extras && tnr ably/ably-asset-tracking && tmux at -t ably'
+alias ably='tnr ably/ably-cocoa && tnr ably/ably-js && tnr ably/ably-extras && tnr ably/ably-asset-tracking && tnr ably/ably-spaces && tmux at -t ably-cocoa'
 
 # For starting multiple sessions in one go.
 alias tnr="${DOTFILES_DIR}/bin/dotfiles-bundle-exec tmuxinator start --attach false"
