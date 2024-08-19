1b4f107f1373ca93d80a0b45c07dbbbb74c6fbad
Lawrence Forooghian <lawrence@forooghian.com>
2024-08-19 09:07:11 +0100

    Add Ably Chat to tmuxinator

--- zsh/alias/other.sh
+++ zsh/alias/other.sh
@@ -4,7 +4,7 @@ alias lsa='ls -alh'
 alias less='less -R' # colors
 
 alias notes='tnr notes && tmux at -t notes'
-alias ably='tnr ably/ably-cocoa && tnr ably/ably-js && tnr ably/ably-extras && tnr ably/ably-asset-tracking && tnr ably/ably-spaces && tmux at -t ably-cocoa'
+alias ably='tnr ably/ably-cocoa && tnr ably/ably-js && tnr ably/ably-extras && tnr ably/ably-asset-tracking && tnr ably/ably-spaces && tnr ably/ably-chat && tmux at -t ably-cocoa'
 
 # For starting multiple sessions in one go.
 alias tnr="${DOTFILES_DIR}/bin/dotfiles-bundle-exec tmuxinator start --attach false"
